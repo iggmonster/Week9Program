8c639b634e14076adfc6680e17a208a1c9767e69
iggmonster <112781295+iggmonster@users.noreply.github.com>
2022-10-30 19:12:29 -0400

    kinda sorta almost works

--- src/Week9Program.java
+++ src/Week9Program.java
@@ -3,7 +3,11 @@ import java.util.Arrays;
 import java.util.Formatter;
 import java.util.List;
 import java.util.function.Predicate;
-
+/*
+@author Keiffer Button
+Lab: L01
+Course: CS1131 Accelerated intro to programming
+ */
 public class Week9Program {
     public static void main(String[] args) {
         Idk obj = new Idk();
