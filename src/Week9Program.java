2b430aba8ae288070ba952456df3f9238b3f8a4d
iggmonster <112781295+iggmonster@users.noreply.github.com>
2022-10-30 19:10:49 -0400

    kinda sorta almost works

--- src/Week9Program.java
+++ src/Week9Program.java
@@ -6,23 +6,34 @@ import java.util.function.Predicate;
 
 public class Week9Program {
     public static void main(String[] args) {
-        ArrayList list1 = new ArrayList<>(Arrays.asList(new String[] {"Larry", "Curly", "Moe", "Shemp", "Curly Joe"}) );
-        System.out.println(new Idk(list1));
+        Idk obj = new Idk();
+        ArrayList list1 = new ArrayList<>(Arrays.asList(new String[] {"Larry", "Curly", "Moe", "Shemp", "Curly Joe", "12345678910"}) );
+                //System.out.println((new Idk<>(list1)));
+        //System.out.println(new Idk(list1));
+      List listsomething = obj.filter( list1, value ->  value.toString().length() == 3 );
+        System.out.printf("filter(%s, value -> value.length() == 3) returns %s\n", list1 ,Idk.filter());
     }
 }
 
 class Idk <T>{
-    public Idk(ArrayList list1) {
+    public Idk() {
 
     }
 
-    List<T> filter (List<T> values, Predicate<? super T> p ){
-        int i = 0;
-        while (true){
-            if (p[i].length < 10){
-
+    List<T> filter (List<T> values, Predicate<? super T> p ) {
+        ArrayList<T> stuff = new ArrayList<>(values);
+        ArrayList<String> big10 = null;
+        List<T> newList = null;
+        for (int i = 0; i < values.size(); i++) {
+            System.out.println(i);
+            if ( ((stuff.get(i)).toString()).length() > 10) {
+                big10.add(String.valueOf(((stuff.get(i)).toString()).length()));
+            }
+            if (p.test(((List<T>)big10).get(i))){
+                newList.add(((List<T>)big10).get(i));
             }
         }
-        return (List<T>) p;
+
+        return newList;
     }
 }
